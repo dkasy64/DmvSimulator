57335cd59f005bdc9138fda2b23e390614916eed
dkasy64 <dawitkasy64@gmail.com>
2024-03-04 16:22:18 -0500

    Completed DmvSimulato

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,24 @@
+import java.security.SecureRandom;
 public class DmvSimulator{
     public static void main(String[]args){
-        System.out.println("First commit");
+        SecureRandom random = new SecureRandom();
+        System.out.println("Welcome to DMV simlator.");
+        int number = random.nextInt(100)+1;
+        System.out.println("Your number is " + number + "\nPlease wait until your number is called.");
+        boolean match = true;
+        int caller = number+ 1;
+        while(match){
+            if(caller > 100){
+                caller = 1;
+            }
+            if(caller == number){
+                System.out.println(caller);
+                System.out.println("You do not have the required paperwork.");
+                match = false;
+            }else{
+                System.out.println(caller);
+                caller++;
+            }
+        }
     }
 }
