040ad5179dd220c4427b1aa965bf2e29ebe0c70a
dkasy64 <dawitkasy64@gmail.com>
2024-03-05 19:49:52 -0500

    miracle addition was added

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -2,6 +2,7 @@ import java.security.SecureRandom;
 public class DmvSimulator{
     public static void main(String[]args){
         SecureRandom random = new SecureRandom();
+        int miracle = random.nextInt(100)+1;
         System.out.println("Welcome to DMV simlator.");
         int number = random.nextInt(200)+1;
         System.out.println("Your number is " + number + "\nPlease wait until your number is called.");
@@ -13,9 +14,14 @@ public class DmvSimulator{
             }
             if(caller == number){
                 System.out.println(caller);
+                if(miracle <= 99){
                 System.out.println("You do not have the required paperwork, idiot.");
                 System.out.println("Go get your papers and come back!");
                 match = false;
+                } else{
+                    System.out.println("You have all the required paperwork. Your all set");
+                    match = false;
+                }
             }else{
                 System.out.println(caller);
                 caller++;
